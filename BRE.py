bb762660ee5bb9130b1c8286396a3d4353274859
Maux82 <a.argentini@gmail.com>
2014-11-05 09:57:32 +0100

    First Commit of BRE

---
+++ BRE.py
@@ -0,0 +1,27 @@
+__author__ = 'maux'
+
+import argparse
+import sys
+
+
+
+def main():
+    # parse command line options
+    try:
+        parser = argparse.ArgumentParser(description='BRE')
+        parser.add_argument('--input', dest='name', action='store', help='specify input list of MS2 peptides ', required=True)
+        args = parser.parse_args()
+
+
+    except parser.error, msg:
+        print msg
+        print "for help use --help"
+        sys.exit(2)
+    # process options
+
+    # process arguments
+    for arg in args:
+        process(arg) # process() is defined elsewhere
+
+if __name__ == "__main__":
+    main()
