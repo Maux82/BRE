04580cc6ab308f570614ea9cc815c8620064ad77
Maux82 <a.argentini@gmail.com>
2014-11-09 00:20:32 +0100

    parser input

--- BRE.py
+++ BRE.py
@@ -3,13 +3,16 @@ __author__ = 'maux'
 import argparse
 import sys
 
-
+def
 
 def main():
     # parse command line options
     try:
         parser = argparse.ArgumentParser(description='BRE')
-        parser.add_argument('--input', dest='name', action='store', help='specify input list of MS2 peptides ', required=True)
+        parser.add_argument('--input', dest='fileInput', action='store', help='specify ranking input file', required=True)
+        parser.add_argument('--typerank', dest='flag_type', action='store', help='Type of ranking Tot.Ranking (1) or Top-k (2) ', required=True)
+        parser.add_argument('--niter', dest='niter',default=1 ,action='store', help='number of Bre iteration > 0. default BRE-1T', required=True)
+
         args = parser.parse_args()
 
 
@@ -19,10 +22,12 @@ def main():
         sys.exit(2)
     # process options
 
-    # process arguments
-    for arg in args:
-        process(arg) # process() is defined elsewhere
-
+    file_name= args.fileInput
+    flag_type= args.flag_type
+    niter =args.niter
+    # get the data
+    ## into BBA
+    ## BRE main loop
 if __name__ == "__main__":
-    print ''
+    print '___'
     main()
